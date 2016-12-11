fc86737cd86f8fc5184407342c3687d4667ba734
ArtyrG <artyr_galeev@mail.ru>
2016-12-11 23:38:56 +0300

    add method accepts a string from the ip and split ( "\\.") in an array of strings

--- address/src/main/java/ip/address/App.java
+++ address/src/main/java/ip/address/App.java
@@ -1,14 +1,49 @@
 package ip.address;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Arthur
  * @version 0.1
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-      
-    }
+public class App {
+	String[] firstIP; // storage for 1 ip
+	String[] secondIP; // storage for 2 ip
+
+	public static void main(String[] args) throws IOException {
+		
+		// for input from the console
+		BufferedReader reader = 
+				new BufferedReader(new InputStreamReader(System.in)); 
+		
+		System.out.println("Input first IP :");
+		//entry in the string 1 ip 
+		String ip1 = reader.readLine(); 
+		System.out.println("Input second IP :");
+		//entry in the string 2 ip 
+		String ip2 = reader.readLine();
+		System.out.println("All ip mask 255.255.255.000 :");
+		
+		App app = new App();
+
+		String[] a1 = app.stringToInt(ip1, app.firstIP);
+		String[] b1 = app.stringToInt(ip2, app.secondIP);
+		
+
+	}
+	
+	// method accepts a string from the ip and split ( "\\.") in an array of strings
+	public String[] stringToInt(String ip, String[] massiveIP) {
+		if (!(ip.isEmpty())) {
+			massiveIP = ip.split("\\.");
+			String[] ad = massiveIP;
+			return ad;
+		} else {
+			String[] ad = { "No input ip!" };
+			return ad;
+		}
+	}
 }
